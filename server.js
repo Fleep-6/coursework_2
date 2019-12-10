c98af30140e2797d227dcd629b75c9a8687ba22a
Fleep-6 <56159619+Fleep-6@users.noreply.github.com>
2019-12-10 13:35:46 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var handleRequest = function(request, response) {
   response.write(host);
   response.end(" | v=1\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+  process.exit(0);
 }
 
 var www = http.createServer(handleRequest);
